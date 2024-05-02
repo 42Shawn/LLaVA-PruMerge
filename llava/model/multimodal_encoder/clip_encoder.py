ef6f3346e8f917395b1b5b955c2a6663e1852b82
42Shawn <36666427+42Shawn@users.noreply.github.com>
2024-05-02 15:04:57 -0500

    update visual encoder

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -282,6 +282,25 @@ class CLIPVisionTower(nn.Module):
 
     
 
+ 
+    @torch.no_grad()
+    def forward(self, images):
+        if type(images) is list:
+            image_features = []
+            for image in images:
+                image_forward_out = self.vision_tower(image.to(device=self.device, dtype=self.dtype).unsqueeze(0), output_hidden_states=True)
+                image_feature = self.feature_select(image_forward_out).to(image.dtype)
+                image_features.append(image_feature)
+        else:
+            # image_forward_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_hidden_states=True)
+            # image_features = self.feature_select(image_forward_outs).to(images.dtype)
+
+            # image_features = self.token_prune_merge_advanced(images, if_adaptive=True, reduction_ratio=1/8)
+            
+            image_features = self.token_prune_merge_advanced_plus(images, if_adaptive=False, reduction_ratio=1/8)
+
+        return image_features
+
     @property
     def dummy_feature(self):
         return torch.zeros(1, self.hidden_size, device=self.device, dtype=self.dtype)
