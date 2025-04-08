a45cc8f5ff6336360ed225460ddf24b28ae31d91
sydpeng <sydpeng1@gmail.com>
2025-04-07 20:31:46 -0400

    pretrian changes

--- scripts/v1_5/pretrain_bitnet.sh
+++ scripts/v1_5/pretrain_bitnet.sh
@@ -1,42 +1,28 @@
 #!/bin/bash
-# export CUDA_VISIBLE_DEVICES='2,3,4,5'
-
-# IMPORTANT: this is the training script for the original LLaVA, NOT FOR LLaVA V1.5!
-
-# Uncomment and set the following variables correspondingly to run this script:
-
-MODEL_VERSION=bitnet-v1.58-3b
-# MODEL_VERSION=llama-2-7b-chat
-
-########### DO NOT CHANGE ###########
-########### USE THIS FOR BOTH ###########
-PROMPT_VERSION=plain
-########### DO NOT CHANGE ###########
-    # --model_name_or_path ./checkpoints/$MODEL_VERSION \
-
 
 deepspeed --include localhost:0,1,2,3,4,5,6,7 llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path lmsys/bitnet-v1.58-3b \
-    --version $PROMPT_VERSION \
+    --model_name_or_path ./bitnet_b1_58_3B \
+    --version v1 \
     --data_path ./playground/data/llava_v1_5_mix665k.json \
     --image_folder ./playground/data \
-    --vision_tower openai/clip-vit-large-patch14 \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ./checkpoints/llava-$MODEL_VERSION-pretrain \
+    --output_dir ./checkpoints/llava-bitnet_b1_58_3B-pretrain \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 16 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
     --save_total_limit 1 \
-    --learning_rate 2e-3 \
+    --learning_rate 1e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
